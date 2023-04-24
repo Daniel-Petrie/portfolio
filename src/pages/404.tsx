97e9b9e99e5f03dd2327c76ec58fdb67d605fe77
Daniel Petrie <danielpetrie07@gmail.com>
2023-04-24 17:37:00 +1200

    initial set up

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <>
+      <div>404</div>
+      <p>Sorry, that page doesn't exist!</p>
+    </>
+  )
+}
+
+export default NotFound
---
+++ src/pages/about.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        temporibus id quod doloribus? Cupiditate amet debitis, ea cumque et
+        accusamus, hic fugiat accusantium laborum ratione similique consequatur
+        placeat reprehenderit. Odio!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        temporibus id quod doloribus? Cupiditate amet debitis, ea cumque et
+        accusamus, hic fugiat accusantium laborum ratione similique consequatur
+        placeat reprehenderit. Odio!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        temporibus id quod doloribus? Cupiditate amet debitis, ea cumque et
+        accusamus, hic fugiat accusantium laborum ratione similique consequatur
+        placeat reprehenderit. Odio!
+      </p>
+    </div>
+  )
+}
+
+export default about
